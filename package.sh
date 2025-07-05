6c5e542de5e8f38e24e4d36b9b74c7f69ad6aea3
Juan Carlos Ruiz <klaufel@gmail.com>
2025-07-05 17:07:06 +0200

    add package

--- package.sh
+++ package.sh
@@ -6,8 +6,9 @@ VERSION=$(grep "## Version:" ItemEra.toc | sed 's/## Version: //')
 # Create the zip file in the current directory
 rm -rf "ItemEra" "ItemEra.zip"
 zip -r "ItemEra.zip" . \
-    -x ".DS_Store" \
-       ".DS_Store" \
+    -x "*.DS_Store" \
+       "*/.DS_Store" \
+       "**/.DS_Store" \
        ".vscode/*" \
        "_Interface/*" \
        "ItemEra/*" \
