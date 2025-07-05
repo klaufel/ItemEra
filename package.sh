a2e6219959c0f452ce13b0cb7f17c96b129b64c8
Juan Carlos Ruiz <klaufel@gmail.com>
2025-07-05 17:06:21 +0200

    add package

---
+++ package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the addon version from TOC file
+VERSION=$(grep "## Version:" ItemEra.toc | sed 's/## Version: //')
+
+# Create the zip file in the current directory
+rm -rf "ItemEra" "ItemEra.zip"
+zip -r "ItemEra.zip" . \
+    -x ".DS_Store" \
+       ".DS_Store" \
+       ".vscode/*" \
+       "_Interface/*" \
+       "ItemEra/*" \
+       "Thumbs.db" \
+       ".git/*" \
+       ".gitignore" \
+       "package.sh" \
+       "*.zip"
+
+echo "Created ItemEra: v${VERSION}.zip"
